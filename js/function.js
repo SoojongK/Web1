d5b071e5e9e6198a6d6275e146591b242d4bbaf2
UlsaNevergiveUp <sksmschlrh123@gmail.com>
2023-01-09 18:18:42 -0700

    update function

--- js/function.js
+++ js/function.js
@@ -68,18 +68,8 @@ $(function(){
     var $list = $('#project>.container>.menu>li>a');
     var $listImg = $('#project>.container>.view>li');
     var $viewOpen = $('.viewOpen');
-    var $viewClose = $('.viewClose');
     var $viewImg = $('#project>.project_bg>.project_img');
-    var $view = $('#project>.project_bg');
   
-    var $dePrev = $('#design>.prev');
-    var $deNext = $('#design>.next');
-    var $design = $('#design>.container>ul');
-    var $designs = $('#design>.container>ul>li');
-    var $gallOpen = $('.gallOpen');
-    var $gallClose = $('.gallClose');
-    var $gall = $('#design>.gallery_bg');
-    var $gallImg = $('#design>.gallery_bg>.gallery_img');
   
     var nowIdx = 0;
   
@@ -109,60 +99,9 @@ $(function(){
       $viewImg.parent().fadeIn();
     });
   
-    $viewClose.on('click',function(event){
-      event.preventDefault();
-      $viewImg.scrollTop(0)
-      $view.fadeOut();
-    });
     
-    $view.on('click',function(){
-      $viewImg.scrollTop(0)
-      $view.fadeOut();
-    });
+    
+    
     //end of project
   
-    //design
-    function galleryMove(){
-      $designs.eq(nowIdx).stop().animate({left:0,},500,function(){
-        $designs.eq(nowIdx).siblings().css({'left':'990px'}).appendTo($design);
-      });
-    }
-  
-    $dePrev.on('click',function(){
-      if(nowIdx>0){
-        nowIdx--;
-      }else{
-        nowIdx = 2;
-      }
-  
-      galleryMove();
-    });
-  
-    $deNext.on('click',function(){
-      if(nowIdx<2){
-        nowIdx++;
-      }else{
-        nowIdx = 0 ;
-      }
-  
-      galleryMove();
-    });
-  
-    $gallOpen.on('click',function(event){
-      event.preventDefault();
-      var src = $(this).attr('href');
-  
-      $gallImg.css({
-        backgroundImage : 'url('+src+')'
-      }).parent().fadeIn();
-    });
-  
-    $gallClose.on('click',function(event){
-      event.preventDefault();
-      $gall.fadeOut();
-    });
-  
-    $gall.on('click',function(){
-      $gall.fadeOut();
-    });//end of design
-  });//end of section handler
+  });
